90f0a53ea60f42b99bf1f2b6088f862b4735d430
huh0529 <huh0529@naver.com>
2021-07-16 23:20:57 +0900

    .

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //id를 자동으로 생성해주는 전략 - identity
     private Long id;
     private String name;
 
